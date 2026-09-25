ca9d81866bfc4cfe7094b653aac81040ef79aece
tiouoo <tiouo@qq.com>
2026-09-25 11:50:59 +0800

    feat(navmenu): 添加导航菜单组件及相关扩展功能
    - 新增 AvaloniaProperty 扩展方法用于批量设置属性值和伪类状态
    - 新增 Binding 扩展方法提供安全绑定功能
    - 实现 NavMenu 控件支持水平折叠、搜索过滤和键盘导航
    - 实现 NavMenuItem 控件支持多级菜单、选中状态和命令执行
    - 添加 OverflowStackPanel 面板用于处理溢出内容显示

---
+++ src/OvoUi/Common/Enums/SearchBoxPlacement.cs
@@ -0,0 +1,7 @@
+namespace OvoUi.Common.Enums;
+
+public enum SearchBoxPlacement
+{
+    Top,
+    Bottom
+}
---
+++ src/OvoUi/Common/Extension/Binding.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Data;
+using OvoUi.Common.Helpers;
+
+namespace OvoUi.Common.Extension;
+
+public static class BindingExtension
+{
+    public static ResultDisposable TryBind(this AvaloniaObject obj, AvaloniaProperty property, BindingBase? binding)
+    {
+        if (binding is null) return new ResultDisposable(new EmptyDisposable(), false);
+        return new ResultDisposable(obj.Bind(property, binding), true);
+    }
+}
